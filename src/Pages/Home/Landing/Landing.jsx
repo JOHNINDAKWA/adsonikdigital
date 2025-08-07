7ae4ee621e0d676bef19ea7da9a48c1cf892159e
John Indakwa <johnindakwa6@gmail.com>
2025-08-07 17:39:48 +0300

    first commit

--- src/Pages/Home/Landing/Landing.jsx
+++ src/Pages/Home/Landing/Landing.jsx
@@ -11,6 +11,7 @@ import {
 } from "react-icons/fa"; // Updated icons for new bubble texts
 
 import "./Landing.css"; // Import the custom CSS for this component
+import { FaArrowRightLong } from "react-icons/fa6";
 
 // Import the user-provided images
 // Ensure these paths are correct relative to where Landing.jsx is located
@@ -127,34 +128,14 @@ const Landing = () => {
             
               <Link to="/services" className="button primary-button">
                 Explore Services
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M9.293 2.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H3a1 1 0 110-2h9.586L9.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                <FaArrowRightLong />
               </Link>
             </div>
 
             <div className="button-group-item">
               <Link to="/partnerships" className="button secondary-button">
                 Get Started
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M9.293 2.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H3a1 1 0 110-2h9.586L9.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+            <FaArrowRightLong />
               </Link>
             </div>
           </div>
